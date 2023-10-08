ac6100edeb67836554e4911a2900b2a3f2ef248c
Marlon Kuzmick <mkuzmick@gmail.com>
2023-10-08 10:08:17 -0400

    Updates:

---
+++ app.js
@@ -0,0 +1,33 @@
+const { App } = require('@slack/bolt');
+const path = require('path');
+const { llog } = require('./src/lib/utils');
+const { noBot } = require('./src/lib/slack/middleware');
+const { handleTesting, handleAll } = require('./src/bot/message-handlers');
+// const { messageHandler, eventHandler, actionHandler, slashHandler } = require('./src/elle-l-bot');  
+
+const everything = /.*/;
+
+require('dotenv').config();
+global.ROOT_DIR = path.resolve(__dirname);
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000
+});
+
+app.message('testing testing', handleTesting);
+app.message(everything, noBot, handleAll);
+
+// app.command('/elle', slashHandler.elleSlash);
+
+// app.event('reaction_added', eventHandler.reactionAdded);
+
+(async () => {
+  // Start your app
+  global.BOT_CONFIG = {channels: [process.env.SLACK_TESTS_CHANNEL]};
+  await app.start();
+  console.log('⚡️ Bolt app is running!');
+})();
