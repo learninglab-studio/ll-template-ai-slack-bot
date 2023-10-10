da9967a608736cc9715f1dc755ef7629c5a6d768
Marlon Kuzmick <mkuzmick@gmail.com>
2023-10-10 17:00:41 -0400

    Updates:

--- src/bot/handle-messages/bot-responses/engage-in-dialogue.js
+++ src/bot/handle-messages/bot-responses/engage-in-dialogue.js
@@ -6,8 +6,6 @@ module.exports.aiDialogueResponseV1 = async ({ client, message, say }) => {
     llog.cyan(llog.divider, `SLACK_AI_CONVERSATION_CHANNEL message`, message)
     let theMessages = await client.conversations.history({channel: message.channel, limit: 20})
     llog.magenta(theMessages)
-
-    
     let messageHistory = theMessages.map(message => {
         if (message.user == process.env.SLACK_BOT_USER_ID) {
             return {role: 'assistant', content: message.text}
@@ -15,33 +13,18 @@ module.exports.aiDialogueResponseV1 = async ({ client, message, say }) => {
             return { role: 'user', content: `${message.user} said ${message.text}` }
         }
     }).reverse(); 
-
     let promptMessages = [
         { 
             role: 'system', 
             content: `you are a highly professional AI assistant who speaks in the manner of Jeeves from the the Jeeves and Wooster series by P.G. Wodehouse.` 
         }, ...messageHistory
     ]
-
     llog.cyan(promptMessages)
-
     let chatCompletion = await openai.chat.completions.create({
         messages: promptMessages,
         model: 'gpt-3.5-turbo-16k',
         max_tokens: 100
     });
     return chatCompletion;
-
-
-
-
-
-
-    let openAiResult = await aiDialogueResponseV1({ messages: result.messages });
-    llog.magenta(openAiResult);
-    let slackResult = await say(openAiResult.choices[0].message.content);
-
-
-
-
+    // let slackResult = await say(openAiResult.choices[0].message.content);
 }
